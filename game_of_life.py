05c845c69e0fa979a88f297fd2b22bdb251098da
Maksym Severyn <maksymko.lette@gmail.com>
2024-07-15 20:57:40 +0300

    Added clear and generate buttons and added frame

--- game_of_life.py
+++ game_of_life.py
@@ -4,12 +4,14 @@ def game(cells):
 
     for n, row in enumerate(cells):
         for n2, cell in enumerate(row):
-            neighbours = count_neightbours(cells, n2, n)
-            if neighbours == 3 and cells[n][n2] == 0:
-                cells2[n][n2] = 1
-            if (neighbours == 2 or neighbours == 3) and cells[n][n2] == 1:
-                cells2[n][n2] = 1
-
+            if cell != 2:
+                neighbours = count_neightbours(cells, n2, n)
+                if neighbours == 3 and cells[n][n2] == 0:
+                    cells2[n][n2] = 1
+                if (neighbours == 2 or neighbours == 3) and cells[n][n2] == 1:
+                    cells2[n][n2] = 1
+            else:
+                cells2[n][n2] = 2
     return cells2
 
 def count_neightbours(cells, x, y):
@@ -17,8 +19,7 @@ def count_neightbours(cells, x, y):
     nearby_cells = [[x-1, y-1], [x, y-1], [x+1, y-1], [x+1, y], [x+1, y+1], [x, y+1], [x-1, y+1], [x-1, y]]
     for x, y in nearby_cells:
         if -1 < x < len(cells[0]) and -1 < y < len(cells):
-            if cells[y][x] != 0:
-
+            if cells[y][x] == 1:
                 neighbours += 1
 
     return neighbours
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 import random
 from game_of_life import game
-import time
+from time import time
 
 pygame.init()
 
@@ -15,7 +15,7 @@ display_surface = pygame.display.get_surface()
 # colors
 black = (0, 0, 0)
 white = (255, 255, 255)
-gray =  (170, 170, 170)
+grey =  (170, 170, 170)
 
 # fps clock
 clock = pygame.time.Clock()
@@ -26,14 +26,29 @@ cell_size = 20
 inbetween_dist = 1
 min_size = 6
 
-# genetare random cells
+# calculate resolution of cells grid
 width_cells_amount = (width * 3)//(cell_size + inbetween_dist)
 height_cells_amount = (height * 3)//(cell_size + inbetween_dist)
 
 # create cells
-probabilities_of_one = 0.6
-cells = [[random.choices([0, 1], [1 - probabilities_of_one, probabilities_of_one])[0] for i in range(width_cells_amount)] for i in range(height_cells_amount)]
+probability_of_one = 0.0
+
+def generate_cells(prob_of_one=0.5):
+    cells = [[random.choices([0, 1], [1 - prob_of_one, prob_of_one])[0] for i in range(width_cells_amount)] for i in range(height_cells_amount)]
+
+    # add layer to cells
+    cells[0] = [2 for i in range(width_cells_amount)]
+    cells[-1] = [2 for i in range(width_cells_amount)]
+    for i in range(len(cells)):
+        cells[i][0] = 2
+        cells[i][-1] = 2
+    
+    return cells
+
+# creating cells grid
+cells = generate_cells(probability_of_one)
 last_cells = [[[0 for i in range(width_cells_amount)] for i in range(height_cells_amount)] for i in range(20)]
+
 
 # offset
 prev_mouse_pos = pygame.mouse.get_pos()
@@ -43,13 +58,22 @@ offset = [width // 2 + prev_mouse_pos[0], height // 2 + prev_mouse_pos[1]]
 wait = 0
 generation = 0
 game_is_stopped = False
-pause1 = time.time()
-pause2 = time.time()
-
+pause1 = time()
+pause2 = time()
+button_down = False
 
 # buttons rectangles
-pause_rect = pygame.Rect(width - 85, 20, 70, 70)
-continue_rect = pygame.Rect(width - 175, 20, 70, 70)
+pause_rect_cords = [width - 85, 20, 70, 70]
+pause_rect = pygame.Rect(*pause_rect_cords)
+
+continue_rect_cords = [width - 175, 20, 70, 70]
+continue_rect = pygame.Rect(*continue_rect_cords)
+
+clear_rect_cords = [width - 175, 107, 160, 50]
+clear_rect = pygame.Rect(*clear_rect_cords)
+
+generate_rect_cords = [width - 175, 174, 160, 50]
+generate_rect = pygame.Rect(*generate_rect_cords)
 
 
 # font
@@ -72,9 +96,11 @@ while True:
 
         # update cells list
         last_cells.pop(0)
-        last_cells.append(cells) 
+        last_cells.append(cells)
 
-    if event.type == pygame.MOUSEBUTTONDOWN:
+
+    # if any of the buttons got clicked
+    if event.type == pygame.MOUSEBUTTONDOWN and time() - pause1 > 0.2:
         mouse_pos = event.pos
         if pause_rect.collidepoint(mouse_pos):
             game_is_stopped = True
@@ -82,34 +108,47 @@ while True:
         elif continue_rect.collidepoint(mouse_pos):
             game_is_stopped = False
         
+        elif clear_rect.collidepoint(mouse_pos):
+            cells = generate_cells(0.0)
+            for _ in range(2):
+                cells = game(cells)
+
+        elif generate_rect.collidepoint(mouse_pos):
+            cells = generate_cells(0.5)
+            cells = game(cells)
+
         else:
-            if time.time() - pause1 > 0.2:
-                row = (offset[1] + mouse_pos[1]) // (cell_size + 1)
-                column = (offset[0] + mouse_pos[0]) // (cell_size + 1)
-                if cells[row][column] == 1:
-                    cells[row][column] = 0
-                else:
-                    cells[row][column] = 1
-                pause1 = time.time()
+            button_down = not button_down
 
-    if keys[pygame.K_SPACE] and time.time() - pause2 > 0.2:
-        game_is_stopped = not game_is_stopped
-        pause2 = time.time()
-
-
+        pause1 = time()
+        
+    # move through the screen with mouse
     if event.type == pygame.MOUSEMOTION:
         current_mouse_pos = pygame.mouse.get_pos()
         for i in range(2):
             offset[i] += (current_mouse_pos[i] - prev_mouse_pos[i])
         prev_mouse_pos = current_mouse_pos
 
-    if keys[pygame.K_w]: # збільшення
+    # drawing cells
+    if button_down:
+        row = (offset[1] + mouse_pos[1]) // (cell_size + 1)
+        column = (offset[0] + mouse_pos[0]) // (cell_size + 1)
+        cells[row][column] = 1
+
+    # start/stop the game
+    if keys[pygame.K_SPACE] and time() - pause2 > 0.2:
+        game_is_stopped = not game_is_stopped
+        pause2 = time()
+
+
+    # changing scale
+    if keys[pygame.K_w]: # scale up
         cell_size += 1
         offset[0] += (width_cells_amount // 2) + (pygame.mouse.get_pos()[0] - width // 2) // 5
         offset[1] += (height_cells_amount // 2) + (pygame.mouse.get_pos()[1] - height // 2) // 5
         wait = 2
 
-    elif keys[pygame.K_s] and cell_size >= min_size: # зменшення
+    elif keys[pygame.K_s] and cell_size >= min_size: # scale down
         cell_size -= 1
         offset[0] -= (width_cells_amount // 2) + (pygame.mouse.get_pos()[0] - width // 2) // 5
         offset[1] -= (height_cells_amount // 2) + (pygame.mouse.get_pos()[1] - height // 2) // 5
@@ -118,11 +157,13 @@ while True:
     if wait:
         wait -= 1
 
-    
+    # drawing all cells
     for n, row in enumerate(cells):
         for n2, cell in enumerate(row):
             if cell == 1:
                 pygame.draw.rect(screen, white, (n2 * (cell_size + inbetween_dist) - offset[0], n * (cell_size + inbetween_dist) - offset[1], cell_size, cell_size))
+            if cell == 2:
+                pygame.draw.rect(screen, grey, (n2 * (cell_size + inbetween_dist) - offset[0], n * (cell_size + inbetween_dist) - offset[1], cell_size, cell_size))
 
             make_blue = 0
             for list in last_cells:
@@ -142,16 +183,24 @@ while True:
 
 
     # generations
-    game_score_text = game_font.render(str(generation), True, 'gray')
-    display_surface.blit(game_score_text, (30, height - 80))
+    generation_text = game_font.render(str(generation), True, 'grey')
+    display_surface.blit(generation_text, (30, height - 80))
 
     # continue and stop buttons
-    pygame.draw.rect(screen, gray, pause_rect)
+    pygame.draw.rect(screen, grey, pause_rect)
     pygame.draw.rect(screen, white, (width - 69, 35, 14, 40))
     pygame.draw.rect(screen, white, (width - 45, 35, 14, 40))
 
-    pygame.draw.rect(screen, gray, continue_rect)
+    pygame.draw.rect(screen, grey, continue_rect)
     pygame.draw.polygon(screen, white, [(width - 158, 37), (width - 123, 54), (width - 158, 74)])
+
+    pygame.draw.rect(screen, grey, clear_rect)
+    clear_text = game_font.render("clear", True, 'white')
+    display_surface.blit(clear_text, (width - 140, 113))
+
+    pygame.draw.rect(screen, grey, generate_rect)
+    generate_text = game_font.render("generate", True, 'white')
+    display_surface.blit(generate_text, (width - 169, 180))
 
 
     pygame.display.update()
